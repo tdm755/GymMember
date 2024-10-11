8233ab78db77f77fcfc991afe7578076e806d8ba
Arpit Tiwari <tdm75513@gmail.com>
2024-10-11 17:26:19 +0530

    .

--- src/components/Trainers/QRCodeOf.jsx
+++ src/components/Trainers/QRCodeOf.jsx
@@ -217,12 +217,12 @@ function TQRCodeOf({setShowQR}) {
         </p>
 
        
-        <button 
+        {/* <button 
           onClick={handleClose}
           className="bg-[#f9f5f5] text-gray-700 px-8 py-2 rounded-full text-lg font-semibold hover:bg-[#f5eeee] transition duration-300 shadow-sm w-full max-w-xs"
         >
           Close
-        </button>
+        </button> */}
       </div>
     </div>
   );
